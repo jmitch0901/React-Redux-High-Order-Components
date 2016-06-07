a5d3f197ef9d1528d146f6bdfaae29dc9d235cb6
Jonathan Mitchell <Jonathan Mitchell>
2016-06-06 21:25:45 -0500

    initial commit

---
+++ src/components/Resources.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default () => {
+  return (
+
+    <div>
+      Super Special Recipe
+      <ul>
+        <li>1 Cup Sugar</li>
+        <li>1 Cup bacon</li>
+        <li>3 Loaves Meatloaf</li>
+      </ul>
+    </div>
+
+
+  );
+};
---
+++ src/components/require_auth.js
@@ -0,0 +1,42 @@
+//NOTE should place Higher ORder Components (HOCs) into
+// own directory under 'components'
+
+
+/*
+  A HIGHER ORDER COMPONENT (or HOC)
+*/
+
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export default function(ComposedComponent){
+
+  class Auth extends Component {
+
+    static contextTypes = {
+      router: React.PropTypes.object
+    }
+
+    componentWillMount(){
+      if(!this.props.authenticated)
+        this.context.router.push('/');
+    }
+
+    componentWillUpdate(nextProps){
+      if(!nextProps.authenticated)
+        this.context.router.push('/');
+    }
+
+    render(){
+      return(
+        <ComposedComponent { ...this.props } />
+      );
+    };
+  };
+
+  function mapStateToProps(state){
+    return { authenticated: state.authenticated };
+  }
+
+  return connect(mapStateToProps)(Auth);
+};
